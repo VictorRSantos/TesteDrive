82d49289177015ac4c50731cd9c8a5a3029b89d6
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2021-05-08 20:33:26 -0300

    Add project files.

---
+++ TesteDrive/TesteDrive/Views/DetalheView.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TesteDrive.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalheView : ContentPage
+    {
+        private const int FREIO_ABS = 800;
+        public string TextoFreioABS { get { return $"Freio ABS - R$ {FREIO_ABS}"; } }
+
+        private const int AR_CONDICIONADO = 1000;
+        public string TextoArCondicionado { get { return $"Ar Condicionado - R$ {AR_CONDICIONADO}"; } }
+
+        private const int MP3_PLAYER = 500;
+        public string TextoMP3Player { get { return $"MP3 Player - R$ {MP3_PLAYER}"; } }
+
+
+        private bool temFreioABS;
+        public bool TemFreioABS
+        {
+            get { return temFreioABS; }
+            set { temFreioABS = value; OnPropertyChanged(nameof(ValorTotal)); }
+
+        }
+
+
+        private bool temArCondicionado;
+
+        public bool TemArCondicionado
+        {
+            get { return temArCondicionado; }
+            set { temArCondicionado = value; OnPropertyChanged(nameof(ValorTotal)); }
+        }
+
+
+        private bool temMP3Player;
+
+        public bool TemMP3Player
+        {
+            get { return temMP3Player; }
+            set { temMP3Player = value; OnPropertyChanged(nameof(ValorTotal)); }
+        }
+
+
+
+        public string ValorTotal
+        {
+            get
+            {
+                return $"Valor Total: R$ {Veiculo.Preco + (TemFreioABS ? FREIO_ABS : 0) + (TemArCondicionado ? AR_CONDICIONADO : 0) + (TemMP3Player ? MP3_PLAYER : 0)}";
+            
+            }
+        }
+
+        public Veiculo Veiculo { get; set; }
+
+        public DetalheView(Veiculo veiculo)
+        {
+            InitializeComponent();                      
+
+            this.Veiculo = veiculo;
+
+            this.BindingContext = this;
+        }
+
+        private void buttonProximo_Clicked(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new AgendamentoView(this.Veiculo));
+
+        }
+    }
+}
---
+++ TesteDrive/TesteDrive/Views/ListagemView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TesteDrive.Views
+{
+
+    public class Veiculo
+    {
+
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Ano { get; set; }
+
+        public string PrecoFormatado { get { return string.Format($"R$ {Preco}"); }}
+    }
+
+    public partial class ListagemView : ContentPage
+    {
+
+        public List<Veiculo> Veiculos { get; set; }
+
+        public ListagemView()
+        {
+            InitializeComponent();
+
+
+            this.Veiculos = new List<Veiculo>()
+            {
+                new Veiculo{Nome="Azera", Preco=60000, Ano=2012},
+                new Veiculo{Nome="Fiesta 2.0", Preco=50000, Ano=2015},
+                new Veiculo{Nome="HB20 S", Preco=40000, Ano=2017}
+
+
+            };
+
+            this.BindingContext = this;
+            
+        }
+
+        private void listviewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+
+            Navigation.PushAsync(new DetalheView(veiculo));
+
+
+        }
+    }
+}
