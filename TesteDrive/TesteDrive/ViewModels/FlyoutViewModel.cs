e731b840884a6b023ba48083957b9b956eeebb16
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2021-06-19 08:25:06 -0300

    Utilizando - IValueConverter, Converters

--- TesteDrive/TesteDrive/ViewModels/FlyoutViewModel.cs
+++ TesteDrive/TesteDrive/ViewModels/FlyoutViewModel.cs
@@ -7,9 +7,18 @@ using Xamarin.Forms;
 
 namespace TesteDrive.ViewModels
 {
-    public class FlyoutViewModel
+    public class FlyoutViewModel : BaseViewModel
     {
-        
+
+        private bool editando = false;
+
+        public bool Editando
+        {
+            get { return editando; }
+            private set { editando = value; OnPropertyChanged(); }
+        }
+
+
         public string Nome
         {
             get { return this.usuario.nome; }
@@ -20,23 +29,60 @@ namespace TesteDrive.ViewModels
         {
             get { return this.usuario.email; }
             set { this.usuario.email = value; }
+        }             
+
+        public string DataNascimento
+        {
+            get { return this.usuario.dataNascimento; }
+            set { this.usuario.dataNascimento = value; }
+        }
+         
+        public string Telefone
+        {
+            get { return this.usuario.telefone; }
+            set { this.usuario.telefone = value; }
         }
 
         private readonly Usuario usuario;
 
         public ICommand EditarPerfilCommand { get; private set; }
 
+        public ICommand SalvarCommand { get; private set; }
+
+        public ICommand EditarCommand { get; private set; }
+
         public FlyoutViewModel(Usuario usuario)
         {
 
             this.usuario = usuario;
 
+            DefinirComandos(usuario);
+
+        }
+
+        private void DefinirComandos(Usuario usuario)
+        {
             EditarPerfilCommand = new Command(() =>
             {
                 MessagingCenter.Send<Usuario>(usuario, "EditarPerfil");
             });
 
-        }
 
+            SalvarCommand = new Command(() =>
+            {
+                this.Editando = false;
+
+                MessagingCenter.Send<Usuario>(usuario, "SucessoSalvarUsuario");
+
+            });
+
+
+            EditarCommand = new Command(() =>
+            {
+                this.Editando = true;
+            
+            });
+
+        }
     }
 }
--- TesteDrive/TesteDrive/Views/FlyoutView.xaml.cs
+++ TesteDrive/TesteDrive/Views/FlyoutView.xaml.cs
@@ -27,18 +27,40 @@ namespace TesteDrive.Views
         {
             base.OnAppearing();
 
-            MessagingCenter.Subscribe<Usuario>(this, "EditarPerfil", (usuario) =>
-            {
-                //Acessar segunda pagina, Editar do TabbedPage
-                this.CurrentPage = this.Children[1];
-            });
+            AssinarMensagens();
         }
+
+      
 
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
 
+            CancelarMensagens();
+        }
+
+
+        private void AssinarMensagens()
+        {
+            MessagingCenter.Subscribe<Usuario>(this, "EditarPerfil", (usuario) =>
+            {
+                //Acessar segunda pagina, Editar do TabbedPage
+                this.CurrentPage = this.Children[1];
+            });
+
+
+            MessagingCenter.Subscribe<Usuario>(this, "SucessoSalvarUsuario", (usuario) =>
+            {
+
+                this.CurrentPage = this.Children[0];
+            });
+        }
+
+        private void CancelarMensagens()
+        {
             MessagingCenter.Unsubscribe<Usuario>(this, "EditarPerfil");
+
+            MessagingCenter.Unsubscribe<Usuario>(this, "SucessoSalvarUsuario");
         }
     }
 }
